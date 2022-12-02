64cf756cb60147ceb5795428930e1688d7042be4
awdrallyracer <65130631+awdrallyracer@users.noreply.github.com>
2022-12-02 17:31:14 +0200

    dialogs wiindows

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import d from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            DIalogs
+        <div className={d.dialogs}>
+            <div className={d.dialogItems}>
+                <div className={d.dialog + ' ' + d.active}>
+                    <NavLink to="/dialogs/1" >Andrii</NavLink>
+                </div>
+                <div className={d.dialog}>
+                    <NavLink to="/dialogs/2">Anton</NavLink>
+                </div>
+                <div className={d.dialog}>
+                    <NavLink to="/dialogs/3">Ilia</NavLink>
+                </div>
+            </div>
+            <div className={d.messages}>
+                    <div className={d.message}>Hi</div>
+                    <div className={d.message}>How are you doing?</div>
+                    <div className={d.message}>lt's drink some cofee tomorrow</div>
+            </div>
         </div>
+
     )
 }
 
